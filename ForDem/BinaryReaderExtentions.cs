0bcb0917728ba45eb611f6a7ec0881a5960752d8
Zan <zan@zendegi.azadi>
2022-11-17 18:09:09 +0330

    Implements ASP.NET Core's `AuthenticationHandler` for Digital Signature Authentication.

---
+++ ForDem/BinaryReaderExtentions.cs
@@ -0,0 +1,9 @@
+﻿namespace ForDem;
+
+public static class BinaryReaderExtentions
+{
+    public static Int32 ReadInt32BE(this BinaryReader binRdr)
+    {
+        return BitConverter.ToInt32(binRdr.ReadBytes(sizeof(Int32)).Reverse().ToArray(), 0);
+    }
+}
