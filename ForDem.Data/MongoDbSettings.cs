176c400f79b0d28dea7f41ab145a98c10e4df06b
Zan Zendegi Azadi <zan@zendegi.azadi>
2022-12-15 16:09:29 +0330

    Archive everything.

--- ForDem.Data/MongoDbSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ForDem.Data;
-
-public sealed class MongoDbSettings
-{
-    public string ConnectionString { get; set; } = default!;
-
-    public string DatabaseName { get; set; } = default!;
-}
