6f62ac23629a5dfdbb3412cfacac722abeadb7e7
Zan Zendegi Azadi <zan@zendegi.azadi>
2022-12-15 17:41:11 +0330

    Add V1::Instance Endpoint.

---
+++ Mastodon/RuleExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using Mastodon.Grpc;
+
+namespace Mastodon;
+
+public static class RuleExtensionMethods
+{
+    public static Grpc.Rule ToGrpc(this Mastodon.Models.Rule i)
+    {
+        return new Rule
+        {
+            Id = i.Id,
+            Text = i.Text,
+        };
+    }
+}
