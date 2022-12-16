70aa001a46517e47f5478bcdb81f6e144edf65ce
Zan Zendegi Azadi <zan@zendegi.azadi>
2022-12-16 19:43:47 +0330

    Take pagination and other params for Directory API.

--- Mastodon.Client/DirectoryClient.cs
+++ Mastodon.Client/DirectoryClient.cs
@@ -12,9 +12,44 @@ public sealed class DirectoryClient
         _client = client;
     }
 
-    public Task<List<Account>?> GetDirectoryAsync()
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="offset">Skip the first n results.</param>
+    /// <param name="limit">How many accounts to load. Defaults to 40 accounts. Max 80 accounts.</param>
+    /// <param name="order">Use active to sort by most recently posted statuses (default) or new to sort by most recently created profiles.</param>
+    /// <param name="local">If true, returns only local accounts.</param>
+    public Task<List<Account>?> GetDirectoryAsync(uint? offset = null, uint? limit = null, string? order = null, bool? local = null)
     {
-        return _client.http.GetFromJsonAsync<List<Account>>("api/v1/directory", MastodonClient._options);
+        var q = new List<string>();
+        if (offset != null && offset != 0)
+        {
+            q.Add($"offset={offset}");
+        }
+
+        if (limit != null && limit != 0)
+        {
+            q.Add($"limit={limit}");
+        }
+
+        if (!string.IsNullOrWhiteSpace(order))
+        {
+            q.Add($"order={order}");
+        }
+
+        if (local != null)
+        {
+            q.Add($"local={local}");
+        }
+
+        var all = string.Join("&", q);
+
+        if (all.Length > 0)
+        {
+            all = $"?{all}";
+        }
+
+        return _client.http.GetFromJsonAsync<List<Account>>($"api/v1/directory{all}", MastodonClient._options);
     }
 }
 
