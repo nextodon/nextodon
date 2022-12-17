bc0c248c79724938cd7a96bda97cd482091915fd
Zan Zendegi Azadi <zan@zendegi.azadi>
2022-12-17 13:33:46 +0330

    Proxy MediaAttachments.

--- Mastodon.Models/MediaAttachment.cs
+++ Mastodon.Models/MediaAttachment.cs
@@ -38,7 +38,7 @@ public sealed partial class MediaAttachment
     /// <summary>
     /// Metadata returned by Paperclip.
     /// </summary>
-    public required MetaHash Meta { get; set; }
+    public required Types.Meta Meta { get; set; }
 
     /// <summary>
     /// Alternate text that describes what is in the media attachment, to be used for the visually impaired or when media attachments do not load.
@@ -56,40 +56,46 @@ public sealed partial class MediaAttachment
     [Obsolete]
     public string? TextUrl { get; set; }
 
-    /// <summary>
-    /// May contain subtrees small and original, as well as various other top-level properties.
-    /// More importantly, there may be another topl-level focus Hash object on images as of 2.3.0, with coordinates can be used for smart thumbnail cropping – see Focal points for cropped media thumbnails for more.
-    /// </summary>
-    public sealed partial class MetaHash
+    public static class Types
     {
-        public string? Length { get; set; }
-        public float? Duration { get; set; }
-        public int? Fps { get; set; }
-        public string? Size { get; set; }
-        public int? Width { get; set; }
-        public int? Height { get; set; }
-        public float? Aspect { get; set; }
-        public string? AudioEncode { get; set; }
-        public string? AudioBitrate { get; set; }
-        public string? AudioChannels { get; set; }
-        public OriginalHash? Original { get; set; }
-        public SmallHash? Small { get; set; }
-
-        public sealed partial class OriginalHash
+        /// <summary>
+        /// May contain subtrees small and original, as well as various other top-level properties.
+        /// More importantly, there may be another topl-level focus Hash object on images as of 2.3.0, with coordinates can be used for smart thumbnail cropping – see Focal points for cropped media thumbnails for more.
+        /// </summary>
+        public sealed partial class Meta
         {
-            public int Width { get; set; }
-            public int Height { get; set; }
-            public string? FrameRate { get; set; }
+            public string? Length { get; set; }
             public float? Duration { get; set; }
-            public int? Bitrate { get; set; }
-        }
-
-        public sealed partial class SmallHash
-        {
-            public int Width { get; set; }
-            public int Height { get; set; }
+            public uint? Fps { get; set; }
             public string? Size { get; set; }
-            public required float Aspect { get; set; }
+            public uint? Width { get; set; }
+            public uint? Height { get; set; }
+            public float? Aspect { get; set; }
+            public string? AudioEncode { get; set; }
+            public string? AudioBitrate { get; set; }
+            public string? AudioChannels { get; set; }
+            public Types.Original? Original { get; set; }
+            public Types.Small? Small { get; set; }
+
+            public static class Types
+            {
+                public sealed partial class Original
+                {
+                    public uint Width { get; set; }
+                    public uint Height { get; set; }
+                    public string? FrameRate { get; set; }
+                    public float? Duration { get; set; }
+                    public uint? Bitrate { get; set; }
+                }
+
+                public sealed partial class Small
+                {
+                    public uint Width { get; set; }
+                    public uint Height { get; set; }
+                    public string? Size { get; set; }
+                    public required float Aspect { get; set; }
+                }
+            }
         }
     }
 }
