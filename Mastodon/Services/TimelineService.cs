a8261049aa3c0671e2727b1a04af2ececb00e708
Zan <zan@zendegi.azadi>
2022-12-18 22:13:20 +0330

    Authorize timeline APIs.

--- Mastodon/Services/TimelineService.cs
+++ Mastodon/Services/TimelineService.cs
@@ -19,24 +19,36 @@ public sealed class TimelineService : Mastodon.Grpc.Timeline.TimelineBase
 
     public override async Task<Grpc.Statuses> GetPublic(Empty request, ServerCallContext context)
     {
+        var authorization = context.GetHttpContext().Request.Headers.Authorization.ToString();
+        _mastodon.SetAuthorizationToken(authorization);
+
         var result = (await _mastodon.Timeline.GetPublicAsync());
         return result.ToGrpc();
     }
 
     public override async Task<Statuses> GetByTag(StringValue request, ServerCallContext context)
     {
+        var authorization = context.GetHttpContext().Request.Headers.Authorization.ToString();
+        _mastodon.SetAuthorizationToken(authorization);
+
         var result = (await _mastodon.Timeline.GetTagAsync(request.Value));
         return result.ToGrpc();
     }
 
     public override async Task<Statuses> GetHome(Empty request, ServerCallContext context)
     {
+        var authorization = context.GetHttpContext().Request.Headers.Authorization.ToString();
+        _mastodon.SetAuthorizationToken(authorization);
+
         var result = (await _mastodon.Timeline.GetHomeAsync());
         return result.ToGrpc();
     }
 
     public override async Task<Statuses> GetList(StringValue request, ServerCallContext context)
     {
+        var authorization = context.GetHttpContext().Request.Headers.Authorization.ToString();
+        _mastodon.SetAuthorizationToken(authorization);
+
         var result = (await _mastodon.Timeline.GetListAsync(request.Value));
         return result.ToGrpc();
     }
@@ -45,6 +57,9 @@ public sealed class TimelineService : Mastodon.Grpc.Timeline.TimelineBase
     [Obsolete]
     public override async Task<Statuses> GetDirect(Empty request, ServerCallContext context)
     {
+        var authorization = context.GetHttpContext().Request.Headers.Authorization.ToString();
+        _mastodon.SetAuthorizationToken(authorization);
+
         var result = (await _mastodon.Timeline.GetDirectAsync());
         return result.ToGrpc();
     }
