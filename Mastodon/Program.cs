65124ead85ee40645686b370c90b65532dc1481c
Zan Zendegi Azadi <zan@zendegi.azadi>
2022-12-18 20:51:55 +0330

    Experimentation with OAuth proxy.

--- Mastodon/Program.cs
+++ Mastodon/Program.cs
@@ -1,8 +1,11 @@
 using Mastodon.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.IdentityModel.Tokens;
+using System.Buffers;
+using System.Net;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -48,29 +51,6 @@ app.UseForwardedHeaders();
 app.UseRouting();
 app.UseGrpcWeb();
 
-app.Use(async (context, next) =>
-{
-    context.Request.EnableBuffering();
-
-    var logger = ((IApplicationBuilder)app).ApplicationServices.GetService<ILogger<Program>>();
-    var content = $"{context.Request.Method}: {context.Request.GetDisplayUrl()}";
-
-    using var stream = context.Request.BodyReader.AsStream(true);
-    using var reader = new StreamReader(stream, Encoding.UTF8, leaveOpen: true);
-
-    var body = await reader.ReadToEndAsync();
-
-    if (body != null && body.Length > 0)
-    {
-        content += "\r\n" + body;
-    }
-
-    logger?.LogInformation(content);
-
-    context.Request.Body.Position = 0;
-    await next();
-});
-
 app.MapGrpcService<AppsService>().EnableGrpcWeb();
 app.MapGrpcService<DirectoryService>().EnableGrpcWeb();
 app.MapGrpcService<InstanceService>().EnableGrpcWeb();
@@ -82,6 +62,129 @@ app.MapGrpcService<TrendsService>().EnableGrpcWeb();
 app.MapGrpcReflectionService().EnableGrpcWeb();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client.");
+
+app.MapPost("/auth/sign_in", async (context) =>
+{
+    var q = context.Request;
+
+    var url = context.Request.GetEncodedUrl();
+    var b = new UriBuilder(url);
+    b.Host = "mastodon.lol";
+    b.Scheme = "https";
+    b.Port = 443;
+
+    var cookieContainer = new CookieContainer();
+    using var handler = new HttpClientHandler { CookieContainer = cookieContainer };
+
+    var client = new HttpClient(handler);
+
+    var cookie = string.Join(";", q.Cookies.Select(x => $"{x.Key}={x.Value}"));
+    try
+    {
+        client.DefaultRequestHeaders.UserAgent.TryParseAdd(context.Request.Headers.UserAgent.FirstOrDefault());
+        client.DefaultRequestHeaders.TryAddWithoutValidation("Cookie", cookie);
+
+        var formData = await context.Request.ReadFormAsync();
+        var formDictionary = formData.ToDictionary(x => x.Key, x => x.Value.FirstOrDefault());
+
+        var form = new FormUrlEncodedContent(formDictionary);
+        form.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(context.Request.ContentType ?? "");
+
+        var response = await client.PostAsync(b.ToString(), form);
+        var content = await response.Content.ReadAsStringAsync();
+        var utf8 = Encoding.UTF8.GetBytes(content);
+
+        var d = context.Response.Cookies;
+        foreach (var cookie0 in cookieContainer.GetAllCookies().ToList())
+        {
+            d.Append(cookie0.Name, cookie0.Value);
+        }
+
+        await context.Response.BodyWriter.WriteAsync(utf8);
+    }
+    catch (Exception ex)
+    {
+        //
+    }
+});
+
+app.MapPost("/oauth/authorize", async (context) =>
+{
+    var q = context.Request;
+
+    var url = context.Request.GetEncodedUrl();
+    var b = new UriBuilder(url);
+    b.Host = "mastodon.lol";
+    b.Scheme = "https";
+    b.Port = 443;
+
+    var cookieContainer = new CookieContainer();
+    using var handler = new HttpClientHandler { CookieContainer = cookieContainer };
+
+    var client = new HttpClient(handler);
+
+    var cookie = string.Join(";", q.Cookies.Select(x => $"{x.Key}={x.Value}"));
+    try
+    {
+        client.DefaultRequestHeaders.UserAgent.TryParseAdd(context.Request.Headers.UserAgent.FirstOrDefault());
+        client.DefaultRequestHeaders.TryAddWithoutValidation("Cookie", cookie);
+
+        var formData = await context.Request.ReadFormAsync();
+        var formDictionary = formData.ToDictionary(x => x.Key, x => x.Value.FirstOrDefault());
+
+        var form = new FormUrlEncodedContent(formDictionary);
+        form.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(context.Request.ContentType ?? "");
+
+        var response = await client.PostAsync(b.ToString(), form);
+        var content = await response.Content.ReadAsStringAsync();
+        var utf8 = Encoding.UTF8.GetBytes(content);
+
+        var d = context.Response.Cookies;
+        foreach (var cookie0 in cookieContainer.GetAllCookies().ToList())
+        {
+            d.Append(cookie0.Name, cookie0.Value);
+        }
+
+        await context.Response.BodyWriter.WriteAsync(utf8);
+    }
+    catch (Exception ex)
+    {
+        //
+    }
+});
+
+app.MapGet("/oauth/authorize", async (context) =>
+{
+    var url = context.Request.GetEncodedUrl();
+    var b = new UriBuilder(url);
+    b.Host = "mastodon.lol";
+    b.Scheme = "https";
+    b.Port = 443;
+
+    var cookieContainer = new CookieContainer();
+    using var handler = new HttpClientHandler { CookieContainer = cookieContainer };
+
+    var client = new HttpClient(handler);
+
+    try
+    {
+        client.DefaultRequestHeaders.UserAgent.TryParseAdd(context.Request.Headers.UserAgent.FirstOrDefault());
+        var content = await client.GetStringAsync(b.ToString());
+        var utf8 = Encoding.UTF8.GetBytes(content);
+
+        var d = context.Response.Cookies;
+        foreach (var cookie in cookieContainer.GetAllCookies().ToList())
+        {
+            d.Append(cookie.Name, cookie.Value);
+        }
+
+        await context.Response.BodyWriter.WriteAsync(utf8);
+    }
+    catch (Exception ex)
+    {
+        //
+    }
+});
 
 app.MapFallback(async context =>
 {
