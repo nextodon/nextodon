4544b1c4f47605e07715b0389cb86ef30e2f7432
Zan Zendegi Azadi <zan@zendegi.azadi>
2022-12-18 22:05:14 +0330

    Proxy Lists.

---
+++ Mastodon/ListExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using Mastodon.Grpc;
+
+namespace Mastodon;
+
+public static class ListExtensionMethods
+{
+    public static Grpc.List ToGrpc(this Mastodon.Models.List i)
+    {
+        return new List
+        {
+            Id = i.Id,
+            RepliesPolicy = i.RepliesPolicy,
+            Title = i.Title,
+        };
+    }
+
+    public static Grpc.Lists ToGrpc(this IEnumerable<Mastodon.Models.List>? i)
+    {
+        var v = new Lists();
+
+        if (i != null)
+        {
+            v.Data.AddRange(i.Select(x => x.ToGrpc()));
+        }
+
+        return v;
+    }
+}
