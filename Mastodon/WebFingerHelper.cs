5944d8785df42920c7b79f451599289edd888971
Zan <zan@zendegi.azadi>
2023-01-18 15:39:42 +0330

    Sometimes we get weird urls.

--- Mastodon/WebFingerHelper.cs
+++ Mastodon/WebFingerHelper.cs
@@ -19,26 +19,30 @@ public static class WebFingerHelper
             return url;
         }
 
-        var u = new UriBuilder(url)
+        try
         {
-            Scheme = "https",
-            Port = 443,
-        };
-        var p = new UriBuilder(preferred ?? "backend.mangoriver-4d99c329.canadacentral.azurecontainerapps.io")
-        {
-            Scheme = "https",
-            Port = 443,
-        };
-
-        if (u.Host == "mastodon.lol")
-        {
-            var builder = new UriBuilder(u.ToString())
+            var u = new UriBuilder(url)
             {
-                Host = p.Host,
+                Scheme = "https",
+                Port = 443,
+            };
+            var p = new UriBuilder(preferred ?? "backend.mangoriver-4d99c329.canadacentral.azurecontainerapps.io")
+            {
+                Scheme = "https",
+                Port = 443,
             };
 
-            url = builder.Uri.ToString();
+            if (u.Host == "mastodon.lol")
+            {
+                var builder = new UriBuilder(u.ToString())
+                {
+                    Host = p.Host,
+                };
+
+                url = builder.Uri.ToString();
+            }
         }
+        catch { }
 
         return url;
     }
