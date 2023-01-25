c4d7ede800e123e3741d206c7359812e8c52a6b6
Zan Zendegi Azadi <zan@zendegi.azadi>
2023-01-25 22:33:55 +0330

    Implements types and classes related to the advanced polls.

--- Mastodon.Data/DataContext.cs
+++ Mastodon.Data/DataContext.cs
@@ -8,6 +8,8 @@ namespace Mastodon.Data;
 public sealed class DataContext
 {
     internal readonly IMongoDatabase database;
+    public readonly IMongoCollection<Poll> Poll;
+
     private static bool inited = false;
 
     static DataContext()
@@ -37,5 +39,40 @@ public sealed class DataContext
     {
         var client = new MongoClient(settings.Value.ConnectionString);
         database = client.GetDatabase(settings.Value.Database);
+
+        Poll = database.GetCollection<Poll>("poll");
+    }
+
+    public async Task VoteAsync(string id, string userId)
+    {
+        var filter = Builders<Poll>.Filter.Eq(p => p.StatusId, id);
+        {
+            var update = Builders<Poll>.Update
+            .SetOnInsert(p => p.StatusId, id)
+            .SetOnInsert(p => p.Kind, PollKind.Weighted)
+            .SetOnInsert(p => p.Votes, new Dictionary<string, Vote>
+            {
+                [userId] = new Vote { UserId = userId, Choices = new List<VoteChoice> { } }
+            });
+
+            var result = await Poll.UpdateOneAsync(filter, update, new UpdateOptions { IsUpsert = true });
+        }
+
+        {
+            var update = Builders<Poll>.Update.Set(p => p.Votes[userId], new Vote { UserId = userId, Choices = new List<VoteChoice> { } });
+            var result = await Poll.UpdateOneAsync(filter, update);
+        }
+    }
+
+    public Task CreatePollAsync(string statusId, PollKind kind)
+    {
+        var poll = new Data.Poll
+        {
+            StatusId = statusId,
+            Kind = kind,
+            Votes = new Dictionary<string, Vote>(),
+        };
+
+        return Poll.InsertOneAsync(poll);
     }
 }
