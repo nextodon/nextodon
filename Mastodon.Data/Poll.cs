00146867e822d3130b9a01d698ea51e6ed5609cc
Zan <zan@zendegi.azadi>
2023-01-25 22:33:55 +0330

    Implements types and classes related to the advanced polls.

---
+++ Mastodon.Data/Poll.cs
@@ -0,0 +1,30 @@
+﻿namespace Mastodon.Data;
+
+[BsonIgnoreExtraElements]
+public sealed class Poll
+{
+    [BsonId]
+    [BsonRequired]
+    [BsonElement("_id")]
+    public required string StatusId;
+    
+    [BsonRequired]
+    public required PollKind Kind;
+
+    [BsonRequired]
+    public required Dictionary<string, Vote> Votes;
+}
+
+[BsonIgnoreExtraElements]
+public sealed class Vote
+{
+    public required string UserId;
+    public required List<VoteChoice> Choices;
+}
+
+[BsonIgnoreExtraElements]
+public sealed class VoteChoice
+{
+    public required uint Choice;
+    public required uint Weight;
+}
