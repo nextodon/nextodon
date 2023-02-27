81f2b7c98b80576fa148799eda36fc20a17532ec
Zan Zendegi Azadi <zan@zendegi.azadi>
2023-02-27 17:49:47 +0330

    Marker APIs implemented.

---
+++ Mastodon/MarkerExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿namespace Mastodon;
+
+public static class MarkerExtensionMethods {
+    public static Grpc.Marker ToGrpc(this Mastodon.Data.Marker i) {
+        var v = new Grpc.Marker {
+            Home = i.Home?.ToGrpc(),
+            Notifications = i.Notifications?.ToGrpc(),
+        };
+
+        return v;
+    }
+
+    public static Grpc.MarkerItem ToGrpc(this Mastodon.Data.Marker.MarkerItem i) {
+        var v = new Grpc.MarkerItem {
+            LastReadId = i.LastReadId,
+            Version = i.Version,
+            UpdatedAt = i.UpdatedAt.ToGrpc(),
+        };
+
+        return v;
+    }
+}
