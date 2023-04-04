74a9053d119cb45d053185fae69a4a6bd3454c79
Zan <zan@zendegi.azadi>
2023-04-04 17:55:02 +0430

    Remove a batch file.

--- generate-db.bat
+++
@@ -1,3 +0,0 @@
-cd Nextodon.Data.PostgreSQL
-dotnet ef dbcontext scaffold --force --context MastodonContext --context-dir . --output-dir Models "Host=localhost;Database=mastodon_production;Username=mastodon;Password=1" Npgsql.EntityFrameworkCore.PostgreSQL
-cd ..
