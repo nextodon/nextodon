ca77f9108304d6dcfac0190f2580ee0aecdea163
Zan <zan@zendegi.azadi>
2023-04-15 16:42:14 +0430

    Read home/public feed from PostgreSQL instead of MongoDb.

---
+++ Nextodon/PostgresExtensions.cs
@@ -0,0 +1,44 @@
+﻿namespace Nextodon;
+
+public static class PostgresExtensions
+{
+    public static async Task< Grpc.Status> ToGrpc(this Data.PostgreSQL.Models.Status i, Data.PostgreSQL.MastodonContext db, ServerCallContext context)
+    {
+        var v = new Grpc.Status
+        {
+            Id = i.Id.ToString(),
+            Text = i.Text,
+            Content = i.Text,
+            SpoilerText = i.SpoilerText,
+            Sensitive = i.Sensitive,
+            CreatedAt = i.CreatedAt.ToGrpc(),
+            EditedAt = i.EditedAt?.ToGrpc(),
+            Language = i.Language,
+            Visibility = Grpc.Visibility.Public,
+        };
+
+        if (i.InReplyToId != null)
+        {
+            v.InReplyToId = i.InReplyToId?.ToString();
+        }
+
+        if (i.InReplyToAccountId != null)
+        {
+            v.InReplyToAccountId = i.InReplyToAccountId?.ToString();
+        }
+
+        if (i.Uri != null)
+        {
+            v.Uri = i.Uri;
+        }
+
+        if (i.Url != null)
+        {
+            v.Url = i.Url;
+        }
+
+        await Task.Yield();
+
+        return v;
+    }
+}
